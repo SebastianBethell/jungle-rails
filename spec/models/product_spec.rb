5ce7ca2b007bf3c4e73f8c00bd859cf4a0842334
Sebastian Bethell <sebbethell@gmail.com>
2017-12-11 20:40:14 +0000

    added testing suite for product validations

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,7 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
+
   describe 'Validations' do
-    # validation tests/examples here
+
+      it 'should create new product if given all four required fields' do
+        @category = Category.create!(name: 'Health')
+        @product = Product.create!(name: 'Plus One HP', price: 1234, quantity: 56, category: @category)
+        expect(@product).to be_valid
+      end
+
+      it 'should not be valid without a name' do
+        @category = Category.create!(name: 'Health')
+        @product = Product.new(name: nil, price: 1234, quantity: 56, category: @category)
+        expect(@product).to_not be_valid
+        expect(@product.errors.full_messages).to include('Name can\'t be blank')
+      end
+
+      it 'should not be valid without a price' do
+        @category = Category.create!(name: 'Health')
+        @product = Product.new(name: 'Plus One HP', price: nil, quantity: 56, category: @category)
+        expect(@product).to_not be_valid
+        expect(@product.errors.full_messages).to include('Price can\'t be blank')
+      end
+
+      it 'should not be valid without a quantity' do
+        @category = Category.create!(name: 'Health')
+        @product = Product.new(name: 'Plus One HP', price: 1234, quantity: nil, category: @category)
+        expect(@product).to_not be_valid
+        expect(@product.errors.full_messages).to include('Quantity can\'t be blank')
+      end
+
+      it 'should not be valid without a category' do
+        @product = Product.new(name: 'Plus One HP', price: 1234, quantity: nil, category: nil)
+        expect(@product).to_not be_valid
+        expect(@product.errors.full_messages).to include('Category can\'t be blank')
+      end
+
   end
+
 end
